22b0f567b9fca5cb6a1b7d456dbf00e0970b3f42
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-07 04:52:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,45 @@
 # require modules here
+require 'pry'
 
-def load_library
+
+def load_library(file_path)
   # code goes here
+  require 'yaml'
+  emoticon = YAML.load_file(file_path)
+  emoticon_newhash = {}
+  emoticon.each do |key, value|
+    emoticon_newhash.store(key, {:english => value[0], :japanese => value[1]})
+  
+    
+  end
+  emoticon_newhash
+
 end
 
-def get_japanese_emoticon
+
+
+def get_japanese_emoticon(file_path, western_emoticon)
   # code goes here
+  
+  emoticon = load_library(file_path)
+  
+  japanese_emoticon = "Sorry, the emoticon was not found"
+  
+  
+
+  binding.pry
+    
+    
+  end 
+  
+  
+  #binding.pry
+
+  
+  
+  
 end
+
 
 def get_english_meaning
   # code goes here
